229fe5df8eca0e6a091cccb69220f8804582353b
vadimmos <vadimmos@ya.ru>
2021-05-19 23:04:43 +0300

    ODA Static server

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,12 @@ import { ODADefinitionProvider } from './scripts/oda-definition-provider';
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.languages.registerDefinitionProvider({ scheme: 'file', language: 'javascript' }, new ODADefinitionProvider()));
+	
+
+	context.subscriptions.push(vscode.commands.registerCommand('oda-tools.Static-server', () => {
+		console.log('ODA Static server');
+	}));
+
 	console.log('"oda-tools" is now active!');
 }
 
